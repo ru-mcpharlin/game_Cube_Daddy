937474ac29c7e7417b3bdb0908c46167363f1aa7
Ru McPharlin <s220749@students.aie.edu.au>
2024-05-06 16:49:38 +0930

    Final Push 06/05/2024

---
+++ Cube Daddy/Assets/Scripts/VirtualCameraController.cs
@@ -0,0 +1,10 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VirtualCameraController : MonoBehaviour
+{
+    [SerializeField] public CameraController.CameraState cameraType;
+    [SerializeField] int index;
+}
