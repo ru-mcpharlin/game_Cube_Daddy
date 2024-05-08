6defb4da979e0fb1462ae5b4bca25e6cb1927caa
DanielDCruzAIE <128771290+DanielDCruzAIE@users.noreply.github.com>
2024-05-08 11:01:00 +0930

    Added Test Animation

--- Cube Daddy/Assets/Scripts/Dan's Dodgy Code/FormingPlatform.cs
+++ Cube Daddy/Assets/Scripts/Dan's Dodgy Code/FormingPlatform.cs
@@ -8,8 +8,14 @@ public class FormingPlatform : MonoBehaviour
     public Transform[] segment;
     public Vector3 positionOffset;
     public float segmentDelay;
-    public float tweenDuration;
-    public AnimationCurve movementTween;
+    public float tweenDuration, loopTweenDuration;
+    public AnimationCurve movementTween, loopTween;
+    public AutoMode autoMode;   
+    public enum AutoMode
+    {
+        none,
+        loop
+    }
     
     // Start is called before the first frame update
     void OnEnable()
@@ -17,6 +23,21 @@ public class FormingPlatform : MonoBehaviour
         for (int i = 0; i < segment.Length; i++)
         {
             segment[i].position += positionOffset;
+        }
+
+        if (autoMode == AutoMode.loop) 
+        {
+            StartCoroutine(Loop()); 
+        }
+    }
+
+    IEnumerator Loop()
+    {
+        for (int i = 0; i < segment.Length; i++)
+        {
+            Vector3 newpos = segment[i].position - positionOffset;
+            Tween.Position(segment[i], newpos, loopTweenDuration, segmentDelay, loopTween, Tween.LoopType.Loop);
+            yield return new WaitForSeconds(segmentDelay);
         }
     }
 
@@ -31,11 +52,9 @@ public class FormingPlatform : MonoBehaviour
         for (int i = 0; i < segment.Length; i++)
         {
             Vector3 newpos = segment[i].position - positionOffset;
-            Tween.Position(segment[i], newpos, tweenDuration, i * segmentDelay, movementTween);
+            Tween.Position(segment[i], newpos, tweenDuration, segmentDelay, movementTween);
             yield return new WaitForSeconds(segmentDelay);
         }
-        
-
     }
 
 }
