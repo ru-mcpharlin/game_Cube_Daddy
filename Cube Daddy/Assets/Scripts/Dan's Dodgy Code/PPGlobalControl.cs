66f1f9ee637bd52e2e414a40fbc7b5c7ac46dac3
DanielDCruzAIE <128771290+DanielDCruzAIE@users.noreply.github.com>
2024-05-26 20:48:34 +0930

    Push before Monday

--- Cube Daddy/Assets/Scripts/Dan's Dodgy Code/PPGlobalControl.cs
+++ Cube Daddy/Assets/Scripts/Dan's Dodgy Code/PPGlobalControl.cs
@@ -26,20 +26,27 @@ public class PPGlobalControl : MonoBehaviour
 
     public void ActivateVolume(int volumeIndex)
     {
+        Debug.Log("Volume Change");
         newVolumeIndex = volumeIndex;
-        volumes[newVolumeIndex].weight = 0;
+        foreach (Volume vol in volumes)
+        {
+            vol.priority = 0;
+        }
+        volumes[newVolumeIndex].weight = 1;
         volumes[newVolumeIndex].priority = 1;
 
-        Tween.Value(0f, 1f, WeightChangeUpdate, blendDuration, 0f, blendTween, Tween.LoopType.None, null , WeightChangeComplete);
+        //Tween.Value(0f, 1f, WeightChangeUpdate, blendDuration, 0f, blendTween, Tween.LoopType.None, null , WeightChangeComplete);
     }
 
     private void WeightChangeUpdate(float weight)
     {
         volumes[newVolumeIndex].weight = weight;
+        /*
         if (currentVolumeIndex != -1)
         {
             volumes[currentVolumeIndex].weight = 1 - weight;
         }
+        */
         
     }
 
